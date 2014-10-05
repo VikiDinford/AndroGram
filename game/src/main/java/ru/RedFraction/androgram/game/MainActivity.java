f9ce1a27f4a69ff12d1608b43b24a8b5b4bc7afe
RedFraction <red.fraction.1998@gmail.com>
2014-10-05 05:21:41 +0400

    Сделано чуть лучше xDDD

--- game/src/main/java/ru/RedFraction/androgram/game/MainActivity.java
+++ game/src/main/java/ru/RedFraction/androgram/game/MainActivity.java
@@ -2,17 +2,14 @@ package ru.RedFraction.androgram.game;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.text.InputFilter;
-import android.text.Spanned;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -20,46 +17,85 @@ import java.util.Random;
 
 public class MainActivity extends Activity {
 
-    private static Random   rnd = new Random();
-    private static int      select;
-    private static String   ready = new String();
-    private static int      points = 0;
-    public static String[]  wordlist = WordLists.RockGroups;
-
-    SharedPreferences       sPref;                                                                  // Сохранение счета
-
-    private EditText et_textEnter;                                                                  //Элементы управления
+    public static String[] wordlist = WordLists.RockGroups;
+    private static String word;
+    private static int select;
+    private static int points = 0;
+    private static Random rnd;
+    //Элементы управления
+    private EditText et_textEnter;
     private TextView tv_word;
     private Button   bt_enter;
     private TextView tv_points;
     private TextView tv_words;
+
+    // Начало метода с входным значением одномерного массива
+    private static String anagram(String[] allWords) {
+        select = rnd.nextInt(allWords.length); // Получаем рандом
+        int lenght = allWords[select].length();
+        boolean[] isChecked = new boolean[lenght];
+
+        for (int i = 0; i < lenght; i++) {
+            int rndChar = rnd.nextInt(allWords[select].length());
+
+            if (isChecked[rndChar] == false) {
+                word += allWords[select].charAt(rndChar);
+                isChecked[rndChar] = true;
+            } else {
+                lenght++;
+            }
+        }
+        return word.toLowerCase();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        InputFilter customFilter = new InputFilter() {
-            @Override
-            public CharSequence filter(CharSequence arg0, int arg1, int arg2, Spanned arg3, int arg4, int arg5){                        //Блокировка Enter
-            for (int i = arg1; i < arg2; i++) {
-                if(arg0.charAt(i) == '\n') {
-                    return "";
+        //Cлушатель кнопки Enter
+        et_textEnter.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                if (event.getAction() == KeyEvent.ACTION_DOWN && (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    Enter();
+                    return true;
                 }
-            }
-                return null;}
-        };
-        et_textEnter.setFilters(new InputFilter[]{ customFilter});
-
-        et_textEnter.setOnKeyListener(new View.OnKeyListener(){
-            public boolean onKey(View v, int keyCode, KeyEvent event){
-            if(event.getAction() == KeyEvent.ACTION_DOWN && (keyCode == KeyEvent.KEYCODE_ENTER)){
-                Enter();
-                return true;
-            }
-            return false;
+                return false;
             }
         });
+
+        View.OnClickListener radioListener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                RadioButton rb = (RadioButton) v;
+                switch (rb.getId()) {
+                    case R.id.rb_1:
+                        wordlist = WordLists.Animes;
+                        Toast.makeText(getApplicationContext(), "Anime", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.rb_2:
+                        wordlist = WordLists.Films;
+                        Toast.makeText(getApplicationContext(), "Films", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.rb_3:
+                        wordlist = WordLists.Games;
+                        Toast.makeText(getApplicationContext(), "Games", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.rb_4:
+                        wordlist = WordLists.RockGroups;
+                        Toast.makeText(getApplicationContext(), "Rock Groups", Toast.LENGTH_SHORT).show();
+                        break;
+                    case R.id.rb_5:
+                        wordlist = WordLists.Countrys;
+                        Toast.makeText(getApplicationContext(), "Country", Toast.LENGTH_SHORT).show();
+                        break;
+
+                    default:
+                        break;
+                }
+            }
+        };
     }
 
     @Override
@@ -72,10 +108,7 @@ public class MainActivity extends Activity {
     public boolean onOptionsItemSelected(MenuItem item) {
 
         switch (item.getItemId()) {                                                                 // Handle item selection
-            case R.id.menu_wordlist:
-                Intent int_wordlist = new Intent(this, SetWordList.class);
-                startActivity(int_wordlist);
-                return true;
+
             case R.id.menu_help:
                 Intent int_help = new Intent(this, Help.class);
                 startActivity(int_help);
@@ -91,26 +124,6 @@ public class MainActivity extends Activity {
                 return super.onOptionsItemSelected(item);
         }
     }
-    
-    // Начало метода с входным значением одномерного массива
-    private static String anagram(String[] allWords){
-        select = rnd.nextInt(allWords.length); // Получаем рандом
-        int lenght = allWords[select].length();
-        boolean[] isChecked = new boolean[lenght];
-
-        for(int i = 0; i < lenght; i++){
-            int rndChar = rnd.nextInt(allWords[select].length());
-
-            if(isChecked[rndChar] == false){
-                ready += allWords[select].charAt(rndChar);
-                isChecked[rndChar] = true;
-            }
-            else{
-                lenght++;
-            }
-        }
-        return ready.toLowerCase();
-    }
 
     private void Enter(){
         if(wordlist[select].toLowerCase().equals(et_textEnter.getText().toString().toLowerCase())){
@@ -118,12 +131,12 @@ public class MainActivity extends Activity {
             tv_word.setText(anagram(wordlist).toLowerCase());
             points++;
             tv_points.setText(String.valueOf(points));
-            ready = "";
+            word = "";
             et_textEnter.setText("");
         }else{
             Toast.makeText(getApplicationContext(), "Неудача -1 очко", Toast.LENGTH_SHORT).show();
             tv_word.setText(anagram(wordlist).toLowerCase());
-            ready = "";
+            word = "";
             points--;
             tv_points.setText(String.valueOf(points));
             et_textEnter.setText("");
@@ -139,7 +152,7 @@ public class MainActivity extends Activity {
     @Override
     protected void onResume() {
         super.onResume();
-
+        //wordlist = Prefs.loadWordlist(getApplicationContext());
         points = Prefs.loadScore(getApplicationContext());
         tv_points.setText(points + "");
     } //Загрузка
